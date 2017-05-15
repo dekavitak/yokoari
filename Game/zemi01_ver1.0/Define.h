3e08d560799270d9df8eb86a41d4ac7ecdf36a1f
yutatakatsu <vocaloid_asax@yahoo.co.jp>
2017-05-15 14:13:37 +0900

    サウンド系を追加

--- Game/zemi01_ver1.0/Define.h
+++ Game/zemi01_ver1.0/Define.h
@@ -14,3 +14,10 @@
 #define BOX_LENGTH 10         // 段ボールの構造体の配列の要素数 
 #define BOX_SPAWN_TIME 60     // 段ボールの出現する時間
 #define BOX_TAKE_LIMIT 5      // 段ボールを１度に持てる数の限界
+
+/* サウンド系 */
+#define BACK_BGN "sound/BGM.mp3"      // 共有BGM
+#define GAME_BGM "sound/Game.mp3"     // ゲームBGM
+#define AITEM_SE "sound/Aitem.mp3"   // アイテム系SE
+#define DAMAGE_SE "sound/Damage.mp3" // 接触時SE
+#define SELECT_SE "sound/Select.mp3" // 選択時SE
